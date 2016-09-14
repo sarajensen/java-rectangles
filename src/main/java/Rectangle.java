791611f5d9f65819431b4d76107f20d9c12dcc96
Sara Jensen <sara@>
2016-09-14 14:37:58 -0700

    save instances of rectangle to an arraylist called instances, which is tested.

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rectangle {
   private int mLength;
   private int mWidth;
+  private static List<Rectangle> instances = new ArrayList<Rectangle>();
 
 
   public Rectangle(int length, int width){
     mLength = length;
     mWidth = width;
-
+    instances.add(this);
   }
 
   public int getLength() {
@@ -15,6 +19,10 @@ public class Rectangle {
 
   public int getWidth() {
     return mWidth;
+  }
+
+  public static List<Rectangle> all() {
+    return instances;
   }
 
   public boolean isSquare() {
--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -32,4 +32,12 @@ public class RectangleTest {
     assertEquals(true, testRectangle.isSquare());
   }
 
+  @Test
+  public void all_returnsAllInstancesOfRectangle_true() {
+    Rectangle firstRectangle = new Rectangle(10, 20);
+    Rectangle secondRectangle = new Rectangle(5, 5);
+    assertTrue(Rectangle.all().contains(firstRectangle));
+    assertTrue(Rectangle.all().contains(secondRectangle));
+  }
+
 }
